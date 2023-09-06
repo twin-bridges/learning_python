3f04efbe93e630c0094ce91b646d83c32aec9c76
Kirk Byers <ktbyers@twb-tech.com>
2023-09-05 20:07:01 -0700

    Data Classes exercise

---
+++ lesson8/exercises/exercise1.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class RouterFacts:
+    hostname: str
+    vendor: str
+    nos: str
+    model: str
+    os_version: str
+    interfaces: List[str]
+    uptime_sec: int
+    serial_number: str
+
+
+xr_rtr = RouterFacts(
+    hostname="la-rtr1",
+    vendor="cisco",
+    nos="iosxr",
+    model="8201-SYS",
+    os_version="7.0.12",
+    interfaces=[
+        "HundredGigE0/0/0/24",
+        "HundredGigE0/0/0/25",
+        "HundredGigE0/0/0/26",
+        "HundredGigE0/0/0/27",
+        "HundredGigE0/0/0/28",
+        "HundredGigE0/0/0/29",
+        "HundredGigE0/0/0/30",
+        "HundredGigE0/0/0/31",
+        "HundredGigE0/0/0/32",
+        "HundredGigE0/0/0/33",
+        "HundredGigE0/0/0/34",
+        "HundredGigE0/0/0/35",
+        "MgmtEth0/RP0/CPU0/0",
+    ],
+    uptime_sec=93073,
+    serial_number="FOC2291AVYB",
+)
+
