e75305549c5d60be04e95920aa9072c6533bd616
Kirk Byers <ktbyers@twb-tech.com>
2023-10-02 14:32:32 -0700

    Lesson10 exercises

--- lesson10/exercises/exercise1.py
+++
@@ -1,9 +0,0 @@
-Create a package named my_pkg.
-
-Add an __init__.py file into the my_pkg directory. The __init__.py should have a single function that prints out your IP address
-
-Modify your $PYTHONPATH such that the directory containing my_pkg is included.
-
-Verify your sys.path has this directory.
-
-Create a program that imports my_pkg and calls my_func()
