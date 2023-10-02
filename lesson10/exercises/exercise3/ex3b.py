af56c4e15bca479fc0dc934f042ab6fe13aacec0
Kirk Byers <ktbyers@twb-tech.com>
2023-10-02 14:54:12 -0700

    Class10, ex3

--- lesson10/exercises/exercise3/ex3b.py
+++
@@ -1,9 +0,0 @@
-import new_pkg
-
-print("\nCalling func1()")
-new_pkg.func1()
-
-print("\nCalling func2()")
-new_pkg.func2()
-
-print()
---
+++ lesson10/exercises/exercise3b/new_pkg/__init__.py
@@ -0,0 +1,5 @@
+from new_pkg.mod1 import func1
+from new_pkg.mod2 import func2
+
+# Remember odd syntax where func1 and func2 are strings
+__all__ = ["func1", "func2"]
